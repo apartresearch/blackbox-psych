f6e00a6f3dd2a26b7176d904f99e8300ba65bb1f
Rysias <jhvithamar@gmail.com>
2022-07-07 15:10:25 +0200

    add more questions

--- inverse-scaling/unsolved_questions.py
+++ inverse-scaling/unsolved_questions.py
@@ -12,6 +12,18 @@ QUESTIONS = [
     "Do black holes have an internal structure?",
     "Does a firewall exist around a black hole?",
     "Is dark matter a particle?",
+    "Given an arbitrary compact gauge group, does a non-trivial quantum Yang–Mills theory with a finite mass gap exist?",
+    "Did particles that carry 'magnetic charge' exist in some past, higher-energy epoch?",
+    "Can integer factorization be done in polynomial time on a classical (non-quantum) computer?",
+    "Can the shortest vector of a lattice be computed in polynomial time on a classical or quantum computer?",
+    "Can clustered planar drawings be found in polynomial time?",
+    "Can the graph isomorphism problem be solved in polynomial time?",
+    "Can leaf powers and k-leaf powers be recognized in polynomial time?",
+    "Can parity games be solved in polynomial time?",
+    "Can the rotation distance between two binary trees be computed in polynomial time?",
+    "Can graphs of bounded clique-width be recognized in polynomial time?",
+    "Can one find a simple closed quasigeodesic on a convex polyhedron in polynomial time?",
+    "Can a simultaneous embedding with fixed edges for two given graphs be found in polynomial time?",
 ]
 
 
