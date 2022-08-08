605387a87e73e1b3c1ce05484ad273fe1c357a49
Rysias <jhvithamar@gmail.com>
2022-08-08 13:07:35 +0200

    first iteration

--- inverse-scaling/saliency.py
+++ inverse-scaling/saliency.py
@@ -2,6 +2,13 @@
 This tests the effects of saliency (https://en.wikipedia.org/wiki/Salience_(neuroscience)#Salience_bias) - a bias to overestimate the prevelance of salient events.
 This will test causes of death.
 """
+import sys
+
+sys.path.append("../")
+from typing import Dict, Iterable
+import src.format_questions as fq
+import pandas as pd
+
 
 PROBABLE_CAUSES = {
     " Ischaemic Heart Disease": 9_000_000,
@@ -42,13 +49,31 @@ def create_question(cause1, cause2) -> str:
     )
 
 
-def main():
-    half_combinations = {
-        (prob, salient): 0 for prob in PROBABLE_CAUSES for salient in SALIENT_CAUSES
-    }
+def get_cause_combinations(
+    probable: Iterable[str], salient: Iterable[str]
+) -> Dict[str, int]:
+    half_combinations = {(prob, sal): 0 for prob in probable for sal in salient}
     reversed_combinations = {(x[1], x[0]): 1 for x in half_combinations}
-    all_combinations = {**half_combinations, **reversed_combinations}
-    print(all_combinations)
+    return {**half_combinations, **reversed_combinations}  # type: ignore
+
+
+def main():
+    all_combinations = get_cause_combinations(PROBABLE_CAUSES, SALIENT_CAUSES)
+    result_list = []
+    for combi, answer_index in all_combinations.items():
+        formatted_question = fq.format_question(
+            "Which cause of death is more probable?", combi
+        )
+        result_list.append(
+            {
+                "prompt": formatted_question,
+                "classes": list(combi),
+                "answer_index": answer_index,
+            }
+        )
+    big_saliency = pd.DataFrame(result_list)
+    big_saliency.to_csv("data/saliency_causes.csv", index=False)
+    big_saliency.sample(n=100).to_csv("data/saliency_causes_sample.csv", index=False)
 
 
 if __name__ == "__main__":
